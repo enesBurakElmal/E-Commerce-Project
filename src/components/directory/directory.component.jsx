419d6657c277f206418e6ea3482384126d90c8fb
Enes Elmal <eneselmal1994@gmail.com>
2022-02-26 00:39:48 +0300

    shop list and collection items update

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,46 +1,14 @@
 import React from 'react'
 import MenuItem from '../menu-item/menu-item.component'
+import SECTIONS_DATA from './sections.data'
 import './directory.styles.scss'
 
 class Directory extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
-      sections: [
-        {
-          title: 'HATS',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats',
-        },
-        {
-          title: 'JACKETS',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets',
-        },
-        {
-          title: 'SNEAKERS',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers',
-        },
-        {
-          title: 'WOMENS',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens',
-        },
-        {
-          title: 'MENS',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens',
-        },
-      ],
+      sections: SECTIONS_DATA,
     }
   }
 
