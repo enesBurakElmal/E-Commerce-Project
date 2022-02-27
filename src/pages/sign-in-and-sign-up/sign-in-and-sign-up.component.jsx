c98df919843be823cc73b20156f69504e3ca1e5f
Enes Elmal <eneselmal1994@gmail.com>
2022-02-27 05:21:47 +0300

    sign in and custom button component/styles created

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-and-sign-up.styles.scss'
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+// render() {
+//     return(
+//         <div className=''>
+//         <SignIn />
+//         </div>
+//     )
+// }
+
+export default SignInAndSignUpPage
