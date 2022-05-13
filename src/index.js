12ecdf051972d8f93a64ac1316b89f4ab9cfee59
Enes Burak Elmal <92387865+eneselmal@users.noreply.github.com>
2022-05-13 12:41:20 +0300

    added styled components package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
+  // <Provider>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  // </Provider>
   document.getElementById('root')
 )
